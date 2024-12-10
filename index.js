c67b52f3e860d2ff32c6b4d6ca01b0d6be4c19d8
Maximo Brito <britofilmmaker@gmail.com>
2024-12-10 07:54:21 -0500

    api string conditionally adjusted to earth_date parameter availability

--- index.js
+++ index.js
@@ -40,7 +40,42 @@ app.get("/mars-photos", async (req, res) => {
 
     const key = process.env.API_KEY;
 
-    const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${earth_date}&camera=${camera}&api_key=${key}`;
+    let apiUrl = "";
+
+    if (earth_date) {
+      apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${earth_date}&camera=${camera}&api_key=${key}`;
+    } else {
+      apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?camera=${camera}&page=1&api_key=${key}`;
+    }
+
+    // Make a request to the third-party API
+    const response = await axios.get(apiUrl);
+
+    // Forward the response data to the client
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.get("/get-single-photo", async (req, res) => {
+  try {
+    // Replace with your third-party API endpoint
+
+    const { camera, rover, earth_date } = req.query;
+
+    console.log({ camera, rover, earth_date });
+
+    const key = process.env.API_KEY;
+
+    let apiUrl = "";
+
+    if (earth_date) {
+      apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${earth_date}&camera=${camera}&api_key=${key}`;
+    } else {
+      apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?camera=${camera}&page=1&api_key=${key}`;
+    }
 
     // Make a request to the third-party API
     const response = await axios.get(apiUrl);
